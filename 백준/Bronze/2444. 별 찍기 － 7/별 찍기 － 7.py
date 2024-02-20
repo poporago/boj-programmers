c6c12849067bb9f29d064ae4347561c773796d4a
poporago <131949171+poporago@users.noreply.github.com>
2024-02-21 03:37:01 +0900

    2번째 풀이 추가

--- 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.py
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.py
@@ -7,3 +7,10 @@ for i in range(1,2*num):
         print('*'* (2*i-1))
     else :
         print(' '*(i-num) + '*'*((2*num-1) -2*(i-num)))
+
+#2
+
+for i in range(1, num):
+    print(' '*(num-i) + '*'*(2*i-1))
+for i in range(num, 0, -1):
+    print(' '*(num-i) + '*'*(2*i-1))
