de7e1e0528b5f6dfa2a58905e32cdcd0658c5912
poporago <131949171+poporago@users.noreply.github.com>
2024-02-22 15:49:10 +0900

    [Bronze II] Title: 문자열 반복, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2675. 문자열 반복/문자열 반복.py
@@ -0,0 +1,10 @@
+num = int(input())
+
+for i in range(num):
+    repeat, word = input().split()
+    repeat = int(repeat)
+    for k in word:
+        for j in range(repeat):
+            print(k, end='')
+    print()
+
